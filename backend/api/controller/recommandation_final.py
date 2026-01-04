f4768b1f1022875bfccc8e69e64e301925525dff
ae-saouiqui <150279358+ae-saouiqui@users.noreply.github.com>
2026-01-04 23:29:09 +0100

    Remove commented-out code and clean up return

--- backend/api/controller/recommandation_final.py
+++ backend/api/controller/recommandation_final.py
@@ -13,10 +13,3 @@ def recommand(recipe:Recipe):
     cursor = products_collection.find({"id":{"$in":ids}})
     products = [Product(**product) for product in cursor]
     return products
-        
-    
-
-
-
-# print(recommand("To make classic fudgy brownies, start by preheating your oven to 350°F (175°C) and greasing or lining an 8x8 inch baking pan. Melt ½ cup of unsalted butter, then stir in 1 cup of sugar, 2 large eggs, and 1 teaspoon of vanilla extract until smooth. In a separate bowl, mix ⅓ cup unsweetened cocoa powder, ½ cup all-purpose flour, ¼ teaspoon salt, and ¼ teaspoon baking powder. Gradually add the dry ingredients into the wet mixture and stir just until combined. If you like, fold in ½ cup of chopped nuts or chocolate chips. Pour the batter into the prepared pan, spread evenly, and bake for 20 to 25 minutes until a toothpick inserted in the center comes out with a few moist crumbs. Let the brownies cool completely before cutting into squares and enjoying!"))
-
